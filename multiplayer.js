27e589931e8b61e631ee9a178ad99b5769afbbb2
Rasmus Eskola <fruitiex@gmail.com>
2014-02-17 23:26:58 +0200

    more keys!

--- multiplayer.js
+++ multiplayer.js
@@ -4,14 +4,29 @@ var io = require('socket.io').listen(8080);
 var sys = require('sys');
 var exec = require('child_process').exec;
 
+// safety first :>
+var getKey = function(data) {
+	switch(data) {
+		case 'Up': return 'Up';
+		case 'Down': return 'Down';
+		case 'Left': return 'Left';
+		case 'Right': return 'Right';
+		case 'a': return 'a';
+		case 'b': return 'b';
+		case 'Return': return 'Return';
+		case 'BackSpace': return 'BackSpace';
+		default: return;
+	}
+};
+
 io.sockets.on('connection', function(socket) {
 	socket.on('keydown', function(data) {
-		console.log("keydown");
-		exec("xdotool keydown a");
+		console.log("keydown " + data);
+		exec("xdotool keydown " + getKey(data));
 	});
 	socket.on('keyup', function(data) {
-		console.log("keyup");
-		exec("xdotool keyup a");
+		console.log("keyup" + data);
+		exec("xdotool keyup " + getKey(data));
 	});
 });
 
